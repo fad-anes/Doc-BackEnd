82d59b1961f4f5bf32333b4c0a75c35d14e998a0
fad-anes <anes.fad@esprit.tn>
2026-04-05 14:36:33 +0100

    chore: initial project setup

---
+++ src/main/java/org/example/pfebackend/PfeBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.pfebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfeBackEndApplication.class, args);
+	}
+
+}
